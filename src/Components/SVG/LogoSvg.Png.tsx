7e4e355954f606302c10bce2d3ee239a128ce30b
Ali.m <iop098aliop098@gmail.com>
2024-12-04 14:06:02 +0330

    creat SVG Component

---
+++ src/Components/SVG/LogoSvg.Png.tsx
@@ -0,0 +1,15 @@
+function LogoSvg() {
+  return (
+    <div>
+      <a href="/">
+        <img
+          src="https://www.upload.ee/image/17269868/_-_Copy__2_-denoised_sharpened_width_400__light-100__wb-25__exposure_correction-removebg-preview.png"
+          className="h-12 "
+          alt="Logo"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default LogoSvg;
