57eff8024539b7fce0b6f17e209af0888f99cb6c
Ali.m <iop098aliop098@gmail.com>
2024-12-08 21:26:09 +0330

    Add Component SVG

--- src/Components/Management User/UserComTotal.tsx
+++ src/Components/Management User/UserComTotal.tsx
@@ -4,6 +4,7 @@ import { getUsers } from "../../Services/Api";
 import TextTitle from "../PropComponents/TextTitle";
 import DeleteSvg from "../SVG/DeleteSvg";
 import EditSvg from "../SVG/EditSvg";
+import SerchSvg from "../SVG/SerchSvg";
 
 function UserComTotal() {
   interface IUser {
@@ -149,21 +150,7 @@ function UserComTotal() {
               onChange={(e) => setSearch(e.target.value)}
             />
             <div className="absolute inset-y-0 end-0 flex items-center pe-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
+              <SerchSvg />
             </div>
           </div>
           <select
