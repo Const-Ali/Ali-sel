40daa58afa97d064e5aad43186e25ee81d3b78d3
Ali.m <iop098aliop098@gmail.com>
2025-05-11 12:23:07 +0330

    UP tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  theme: {
     extend: {
       fontFamily: {
         sans: ["Nazanin", "sans-serif"],
+      },
+      animation: {
+        shimmer: "shimmer 1.5s infinite",
+      },
+      keyframes: {
+        shimmer: {
+          "0%": { backgroundPosition: "-200% 0" },
+          "100%": { backgroundPosition: "200% 0" },
+        },
       },
     },
   },
