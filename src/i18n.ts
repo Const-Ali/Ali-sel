c15f2380f475afa5f27e2c7f49faff37268b52cd
Ali.m <iop098aliop098@gmail.com>
2025-05-11 12:23:58 +0330

    UP i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,20 +1,20 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
 
 i18n
-  .use(Backend)
+  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en",
-    debug: true,
-    interpolation: {
-      escapeValue: false,
-    },
+    fallbackLng: "fa",
+    debug: false,
     backend: {
       loadPath: "/locales/{{lng}}/translation.json",
+    },
+    interpolation: {
+      escapeValue: false,
     },
   });
 
